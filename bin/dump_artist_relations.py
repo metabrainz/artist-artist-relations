b42a62a146d3bc405b21553fc074808bc6f4a15f
Robert Kaye <rob@musicbrainz.org>
2019-11-15 16:05:58 +0000

    Minor fixes and getting a-a on par with ac-ac

--- bin/dump_artist_relations.py
+++ bin/dump_artist_relations.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
-import json
 import psycopg2
 from psycopg2.errors import OperationalError, DuplicateTable
+import bz2
+import ujson
 
-DUMP_QUERY = '''SELECT count, a0.gid, a0.name, a1.gid, a1.name
+DUMP_QUERY = '''SELECT CAST(count AS float) / (select max(count) from artist_artist_relations) as score,
+                       a0.gid, a0.name, a1.gid, a1.name
                   FROM artist_artist_relations arr
                   JOIN artist a0 ON arr.artist_0 = a0.id
                   JOIN artist a1 ON arr.artist_1 = a1.id
+                 WHERE count > 3
+              ORDER BY score DESC
              '''
 
-def dump_table():
-    with psycopg2.connect('dbname=musicbrainz_db user=musicbrainz host=musicbrainz-docker_db_1 password=musicbrainz') as conn:
-        with conn.cursor() as curs:
-            curs.execute(DUMP_QUERY)
-            while True:
-                row = curs.fetchone()
-                if not row:
-                    return None
+def dump_table(filename):
+    with bz2.open(filename, "wt") as f:
+        with psycopg2.connect('dbname=musicbrainz_db user=musicbrainz host=musicbrainz-docker_db_1 password=musicbrainz') as conn:
+            with conn.cursor() as curs:
+                curs.execute(DUMP_QUERY)
+                while True:
+                    row = curs.fetchone()
+                    if not row:
+                        return None
 
-                print(json.dumps({
-                    'count' : row[0],
-                    'artist_mbid_0' : row[1],
-                    'artist_name_0' : row[2],
-                    'artist_mbid_1' : row[3],
-                    'artist_name_1' : row[4]
-                }))
+                    f.write(ujson.dumps({
+                        'score' : row[0],
+                        'mbid_0' : row[1],
+                        'name_0' : row[2],
+                        'mbid_1' : row[3],
+                        'name_1' : row[4]
+                    }) + "\n")
 
 
 if __name__ == "__main__":
-    dump_table()
+    dump_table("artist_artist_relations.json.bz2")
